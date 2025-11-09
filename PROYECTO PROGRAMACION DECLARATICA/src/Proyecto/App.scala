d66238aab4eb490be68f96bbbd792daff89cb421
marcosrodriguezropero <marcosrodriguezropero@gmail.com>
2025-11-10 00:27:07 +0100

    Refactor tablero printing for better formatting

--- PROYECTO PROGRAMACION DECLARATICA/src/Proyecto/App.scala
+++ PROYECTO PROGRAMACION DECLARATICA/src/Proyecto/App.scala
@@ -98,11 +98,11 @@ object TableroClasicoLyS extends TableroJuego:
 
   override def pintarTablero(estado: Estado): Unit = //metodo para pintar el tablero
     val s = pintarNodo(_, estado)
-    println(s" ${s(I1A)}-----${s(MA)}-----${s(D1A)}")
-    println(" ╱ | \\ | / | \\")
-    println(s" ${s(I2M)}---${s(I1M)}-----${s(MM)}-----${s(D1M)}---${s(D2M)}")
-    println(" \\ | / | \\ | /")
-    println(s" ${s(I1B)}-----${s(MB)}-----${s(D1B)}")
+    println(s"         ${s(I1A)}-----${s(MA)}-----${s(D1A)}")
+    println("      ╱  |  \\  |  /  |  \\")
+    println(s"     ${s(I2M)}---${s(I1M)}-----${s(MM)}-----${s(D1M)}---${s(D2M)}")
+    println("      \\  |  /  |  \\  |  /")
+    println(s"         ${s(I1B)}-----${s(MB)}-----${s(D1B)}")
 
 
   override def esFinPartida(estado: Estado): Option[Jugador] = { //metodo que devuelve el ganador del juego si existe, sino devuelve None
